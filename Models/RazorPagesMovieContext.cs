95e030116bff5d75d295e7ee367ea785ca411515
Maureen Brennan <mbbrennan@gmail.com>
2020-10-19 20:21:54 -0600

    Add project files.

---
+++ Models/RazorPagesMovieContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie19Oct2020.Models
+{
+    public class RazorPagesMovieContext :DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options) : base(options)
+        {
+        }
+
+        public RazorPagesMovieContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Database_Connection"));
+        }
+        public DbSet<RazorPagesMovie19Oct2020.Models.Movie> Movie { get; set; }
+    }
+}
